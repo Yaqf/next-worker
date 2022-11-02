f2eda15bd6ab88c4dc3d60961858148fef3f1c30
yaqf <yeovng@gmail.com>
2022-11-02 16:00:31 +0800

    feat: with web worker

---
+++ worker.ts
@@ -0,0 +1,18 @@
+import SparkMD5 from "spark-md5";
+import {FileProps} from "./pages";
+
+const handleFileHash = (chunkList: FileProps[]) => {
+    const spark = new SparkMD5();
+    return chunkList.map((item) => {
+        spark.appendBinary(item.file as unknown as string);
+        return {
+            ...item,
+            hash: spark.end()
+        }
+    })
+}
+addEventListener('message', (e) => {
+    const chunkList = e.data;
+    postMessage(handleFileHash(chunkList))
+})
+
